30670d46e14f8e3aa65aca1ff9693f42f879e9d4
sweetlifemw <sweetlifemw6@gmail.com>
2025-07-20 02:00:50 -0700

    server.js

--- server.js
+++ server.js
@@ -13,12 +13,19 @@ app.use(express.json());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+if (!process.env.MONGO_URI) {
+  console.error(" MONGO_URI is not set in environment variables!");
+  process.exit(1);
+}
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log(" Connected to MongoDB"))
-.catch((err) => console.error("MongoDB connection failed:", err));
+.catch((err) => console.error(" MongoDB connection failed:", err));
+
 
 app.use(session({
   secret: "simpleappsecret",
